b193675b58de4654e652b9af4fd62c04caabd95e
Mosich <m.chegeni7979@gmail.com>
2020-02-09 17:51:10 +0330

    adding the register-verify route | adding some of register-verify in auth controller fanctionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,6 +23,22 @@ class AuthController extends Controller
 
         // TODO: sending message through email/mobile to the user for completing THE 'registration'
         Log::info('SENDING-REGISTER-CODE-MESSAGE-TO-USER', ['code' => $code]);
-        return response(['message' => 'کاربر با موفقیت ثبت موقت شد'], 200);
+        return response(['message' => 'کاربر با موفقیت ثبت موقت شد', 'code' => $code], 200);
+    }
+
+    /**
+     * @param $code
+     * @param $data_key
+     */
+    public function register_verify($code, $data_key)
+    {
+        // TODO: decide that cached data most be destroyed after being used or wait for expiration. P.S. : for now we wait for expiration
+        $register_data = Cache::get('user-auth-register-' . $data_key);
+
+        if ($register_data && $register_data['code'] == $code) {
+            dd("success!", $code, $data_key, $register_data);
+        } else {
+            dd($code, $data_key, $register_data);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ Route::group(["namespace" => "\Laravel\Passport\Http\Controllers"], function ($r
 
 Route::post('register', 'AuthController@register')->name('register');
 
+Route::get('register-verify/{code}/{data_key}', 'AuthController@register_verify')->name('register-verify');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
