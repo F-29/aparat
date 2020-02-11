18ee6433c491019e172c8fb47383c752e999ed9b
Mosich <m.chegeni7979@gmail.com>
2020-02-11 21:04:43 +0330

    deleting unnecessary comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-//use App\Exceptions\RegisterVerificationException; // only with cache registration method
 use App\Exceptions\UserAlreadyExistsException;
 use App\Http\Requests\Auth\RegisterNewUserRequest;
 use App\Http\Requests\Auth\RegisterVerifyUserRequest;
@@ -40,10 +39,6 @@ class AuthController extends Controller
         } catch (Exception $e) {
             throw new UserAlreadyExistsException($e);
         }
-//        $expiration = config('auth.register_cache_expiration', 1440); // only with cache registration method
-
-//        Cache::put('user-auth-register-' . $value, compact('type', 'code'), $expiration); // only with cache registration method
-
         // TODO: sending message through email/mobile to the user for completing THE 'registration'
         Log::info('SENDING-REGISTER-CODE-MESSAGE-TO-USER', ['code' => $code]);
         return response(['message' => 'کاربر با موفقیت ثبت موقت شد', 'code' => $code], 200);
@@ -72,12 +67,5 @@ class AuthController extends Controller
         $user->save();
 
         return response($user, 200);
-//        // TODO: decide that cached data most be destroyed after being used or wait for expiration. P.S. : for now we wait for expiration
-//        $register_data = Cache::get('user-auth-register-' . $data_key); // only with cache registration method
-
-//        if ($register_data && $register_data['code'] == $code) {
-//              // only with cache registration method
-//        }
-//        throw new RegisterVerificationException('something went wrong check email and verify code again'); // only with cache registration method
     }
 }
